ab71a2b3c512afd7b243e85d0aee8fe11a4959f5
pangolinfor <pangolinfor@aliyun.com>
2015-08-20 16:43:30 +0800

    自己的下载下来的工程-二次修改-再提交。

---
+++ src/com/letus/cheetah/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.letus.cheetah;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+
+public class HelloWorld extends ServletRequestContext {
+
+	public HelloWorld(HttpServletRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+}
